497bd710d4d2d1fb969db46c3fbe55c438d85589
choi-jiae <96973901+choi-jiae@users.noreply.github.com>
2024-04-04 22:54:10 +0900

    week 5

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import TOC from './components/TOC';
 import ReadContent from './components/ReadContent';
 import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 import Subject from './components/Subject';
 import Control from './components/Control';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.max_content_id = 3; // ui에 영향을 주지 않는 데이터는 state에 넣을 필요가 X. 불필요한 렌더링이 생길수도 있어서!
         this.state = {
-            mode: 'create',
+            mode: 'welcome',
             selected_content_id: 2,
             subject: {title: 'WEB', sub: 'world wide web!'},
             welcome: {title: 'Welcome', desc: 'Hello, React!!'},
@@ -21,30 +23,61 @@ class App extends Component {
             ]
         }
     }
-    render() {
-        console.log('App render');
+    getReadContent(){
+        var i = 0;
+        while(i < this.state.contents.length) {
+            var data = this.state.contents[i];
+            if (data.id === this.state.selected_content_id) {
+                return data;
+            }
+            i = i + 1;
+        }
+
+    }
+    getContent(){
         var _title, _desc, _article = null;
         if (this.state.mode === 'welcome') {
             _title = this.state.welcome.title;
             _desc = this.state.welcome.desc;
             _article = <ReadContent title={_title} desc={_desc}></ReadContent>
         } else if (this.state.mode === 'read') {
-            var i = 0;
-            while(i < this.state.contents.length) {
-                var data = this.state.contents[i];
-                if (data.id === this.state.selected_content_id) {
-                    _title = data.title;
-                    _desc = data.desc;
-                    break;
-                }
-                i = i + 1;
-            }
-            _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+            var _content = this.getReadContent();
+            _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
         } else if (this.state.mode === 'create') {
             _article = <CreateContent onSubmit={function(_title, _desc){
-                console.log(_title, _desc);
+                this.max_content_id = this.max_content_id + 1;
+                var _contents = Array.from(this.state.contents);
+                _contents.push({id: this.max_content_id, title: _title, desc: _desc});
+                this.setState({
+                    contents: _contents,
+                    mode: 'read',
+                    selected_content_id: this.max_content_id
+                });
+                   
             }.bind(this)}></CreateContent>
-        } 
+        } else if (this.state.mode === 'update') {
+            _content = this.getReadContent();
+            _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc){
+                var _contents = Array.from(this.state.contents);
+                var i = 0;
+                while(i < _contents.length) {
+                    if (_contents[i].id === _id) {
+                        _contents[i] = {id: _id, title: _title, desc: _desc};
+                        break;
+                    }
+                    i = i + 1;
+                }
+                this.setState({
+                    contents: _contents,
+                    mode: 'read'
+                });
+            }.bind(this)}></UpdateContent>
+        }
+        return _article;
+    }
+    render() {
+        console.log('App render');
+
         return (
             <div className="App">
                 <Subject 
@@ -64,11 +97,29 @@ class App extends Component {
                 data={this.state.contents}>
                 </TOC>
                 <Control onChangeMode = {function(_mode){
+                    if (_mode === 'delete'){
+                        if (window.confirm('really?')){
+                            var _contents = Array.from(this.state.contents);
+                            var i = 0;
+                            while (i < _contents.length) {
+                                if (_contents[i].id === this.state.selected_content_id) {
+                                    _contents.splice(i, 1); // i번째부터 1개를 지워라
+                                    break;
+                                }
+                                i = i + 1;
+                            }
+                            this.setState({
+                                mode: 'welcome',
+                                contents: _contents
+                            });
+                            alert('deleted!');
+                        }
+                    }
                     this.setState({
                         mode: _mode
                     });
                 }.bind(this)}></Control>
-                {_article}
+                {this.getContent()}
             </div>
         );
     }
